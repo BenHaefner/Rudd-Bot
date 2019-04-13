fa0020f5982538a58c756164dd2d54ee3c759244
BenHaefner <ibenroy2@gmail.com>
2019-04-13 16:16:56 -0400

    convert to discord.py v 1.0

--- cogs/quote.py
+++ cogs/quote.py
@@ -3,11 +3,11 @@ import discord
 from discord.ext import commands
 
 
-class Quote():
+class Quote(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-    
+        
     @commands.command(name='grab',
                 description="Saves a quote.",
                 pass_context=True)
@@ -17,22 +17,22 @@ class Quote():
         try:
             with open("quotes.txt", 'a') as myfile:
                 myfile.write(msg)
-            await self.client.say('Quote saved')
+            await context.message.channel.send('Quote saved')
         except Exception:
-            await self.client.say('Quote could not be saved')
+            await context.message.channel.send('Quote could not be saved')
 
 
     @commands.command(name='quote',
                     description="Get a quote.",
                     pass_context=True)
-    async def quote(self,context):
+    async def quote(self, context):
         try:
             with open("quotes.txt", 'r') as myfile:
                 msgArray = []
                 for line in myfile:
                     msgArray.append(line)
                 msg = msgArray[random.randint(0, len(msgArray)-1)]
-                await self.client.say(msg)
+                await context.message.channel.send(msg)
         except Exception:
             print("Something went wrong")
 
--- rudd-bot.py
+++ rudd-bot.py
@@ -1,19 +1,18 @@
-import asyncio
+import asyncio, config, discord
 from discord import Game
 from discord.ext.commands import Bot
-import config
 
 BOT_PREFIX = ("$")
 TOKEN = config.Token()
 
 client = Bot(command_prefix=BOT_PREFIX)
 
-startup_extensions = ["cogs.quote"]
+startup_extensions = ["quote"]
 
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name=" Bobby Newport in P&R"))
+    await client.change_presence(activity=Game(name=" Bobby Newport in P&R"))
     print("Logged in as " + client.user.name)
 
 
@@ -21,15 +20,16 @@ async def list_servers():
     await client.wait_until_ready()
     while not client.is_closed:
         print("Current servers:")
-        for server in client.servers:
-            print(server.name)
+        for server in client.guilds:
+            print(str(server))
         await asyncio.sleep(600)
 
 
 client.loop.create_task(list_servers())
+
 for extension in startup_extensions:
     try:
-        client.load_extension(extension)
+        client.load_extension('cogs.' + extension)
     except Exception as e:
         exc = '{}: {}'.format(type(e).__name__, e)
         print('Failed to load extension {}\n{}'.format(extension, exc))
