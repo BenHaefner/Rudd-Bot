51cb54e2421d23921ceffb32cca66a525c4b58fc
BenHaefner <ibenroy2@gmail.com>
2019-04-28 18:44:31 -0400

    Changed clear analytics file so it will wipe data properly
    Used Crontab to do this once a month

--- clear_analytics.py
+++ clear_analytics.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-conn = sqlite3.connect('rudd.db')
+conn = sqlite3.connect('Rudd-Bot/rudd.db')
 c = conn.cursor()
-c.execute("DELETE FROM played_games")
-c.execute("DELETE FROM played_songs")
+c.execute('DELETE FROM played_games')
+c.execute('DELETE FROM played_songs')
 conn.commit()
 conn.close()
