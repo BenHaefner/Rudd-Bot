2f6a185e60d6b426a00d35ae7657512d5a538f79
BenHaefner <ibenroy2@gmail.com>
2020-03-01 01:13:35 -0500

    I need to fix everything involved in this program

--- cogs/quote.py
+++ cogs/quote.py
@@ -14,8 +14,8 @@ class Quote(commands.Cog):
         self.members = cogs.users.get_users()
         cogs.users.refresh_users(client)
 
-    @commands.command(name='grab',
-                description='Saves the last message send.',
+    @commands.command(name="grab",
+                description="Saves the last message send.",
                 pass_context=True)
     async def grab(self, context):
         try:
@@ -26,18 +26,18 @@ class Quote(commands.Cog):
             for i, message in enumerate(history):
                 if onNext is True:
                     author = str(message.author.id)
-                    args = '<@' + author + '> ' + message.content.replace('grab ', '')
+                    args = "<@" + author + "> " + message.content.replace("grab ", "")
                     await context.message.channel.send(add_quote(self.members,args))
                     break
                 if message.id == msg.id:
                     onNext = True
         except Exception as e:
-            await context.message.channel.send('Quote could not be saved')
+            await context.message.channel.send("Quote could not be saved")
             print(e)
 
 
-    @commands.command(name='quote',
-                    description='Get a quote, or quote a user.',
+    @commands.command(name="quote",
+                    description="Get a quote, or quote a user.",
                     pass_context=True)
     async def quote(self, context, *args):
         try:
@@ -45,71 +45,71 @@ class Quote(commands.Cog):
             if (len(args) is 0):
                 await context.message.channel.send(get_rand_quote())
             else:
-                msg = ' '.join(args)
+                msg = " ".join(args)
                 await context.message.channel.send(add_quote(self.members,msg))
         except Exception as e:
-            await context.message.channel('You are doing something unsafe. If you have an apostraphy or quotation mark... I dont like that shit.')
+            await context.message.channel("You are doing something unsafe. If you have an apostraphy or quotation mark... I dont like that shit.")
             print(e)
     
-    @commands.command(name='lookup',
-                description='Gets a users quote.',
+    @commands.command(name="lookup",
+                description="Gets a users quote.",
                 pass_context=True)
     async def lookup(self, context, args):
         try:
             self.members = cogs.users.get_users()
             if (len(args) is None):
-                await context.message.channel.send('Specify a user to lookup a quote for.')
+                await context.message.channel.send("Specify a user to lookup a quote for.")
             else:
                 await context.message.channel.send(get_rand_quote_of_user(self.members,args))
         except Exception as e:
-            await context.message.channel.send('Something went wrong')
+            await context.message.channel.send("Something went wrong")
             print(e)
 
 
 def add_quote(members, args):
-    user = args.split()[0].strip().replace('!','')
+    user = args.split()[0].strip().replace("!","")
     if "@" not in user:
-        return 'Quote could not be added, please include a user'
+        return "Quote could not be added, please include a user"
     if user not in members:
-        return 'That is not currently a member of this or any server I know.'
+        return "That is not currently a member of this or any server I know."
     message = '"' + ' '.join(args.split()[1:]) + '"'
-    if message is '':
-        return 'That quote contained no message!'
+    if message is "":
+        return "That quote contained no message!"
     q = (user,message)
-    conn = sqlite3.connect('rudd.db')
+    conn = sqlite3.connect("rudd.db")
     c = conn.cursor()
-    c.execute('INSERT INTO quotes VALUES (?,?)', q)
+    c.execute("INSERT INTO quotes VALUES (?,?)", q)
     conn.commit()
     conn.close()
-    return 'Quote added.'
+    return "Quote added."
 
 
 def get_rand_quote():
-    conn = sqlite3.connect('rudd.db')
+    conn = sqlite3.connect("rudd.db")
     c = conn.cursor()
-    c.execute('SELECT * From quotes')
+    c.execute("SELECT * From quotes")
     result = c.fetchall()
     result = result[random.randint(0, len(result) - 1)]
     name = cogs.users.get_name_from_id(result[0])
-    message = ' '.join(result[1:])
+    message = " ".join(result[1:])
     result = name + ": " + message
     conn.close()
     return result 
 
 
 def get_rand_quote_of_user(members, args):
-    args = args.replace('!','')
+    args = args.replace("!","")
     if "@" not in args:
-        return 'That isnt a user!'
+        return "That isnt a user!"
     if args not in members:
-        return 'That is not currently a member of this or any server I know.'
-    conn = sqlite3.connect('rudd.db')
+        return "That is not currently a member of this or any server I know."
+    conn = sqlite3.connect("rudd.db")
     c = conn.cursor()
-    c.execute('SELECT * From quotes WHERE user = (?)', (args,))
+    c.execute("SELECT * From quotes WHERE user = (?)", (args,))
     result = c.fetchall()
     result = result[random.randint(0, len(result) - 1)]
     name = cogs.users.get_name_from_id(result[0])
-    message = ' '.join(result[1:])
+    message = " ".join(result[1:])
     result = name + ": " + message
     conn.close()
     return result
